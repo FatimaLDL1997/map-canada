313144564a190f0f4c724bcfb6bc52d809bf0c07
Fatima Labade <fatima.labade@ontariotechu.net>
2023-02-20 19:12:11 -0500

    prov

--- src/data.js
+++ src/data.js
@@ -18,77 +18,72 @@ export const landmarks = [
   },
   {
     id: 3,
-    province: "Manitoba",
+    province: "Newfoundland and Labrador",
     title: "title3",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
     image: "/images/road.jpg",
   },
   {
     id: 4,
-    province: "Manitoba",
+    province: "Alberta",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 5,
+    province: "New Brunswick",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 6,
+    province: "Alberta",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 7,
+    province: "Nova Scotia",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 8,
+    province: "Ontario",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 9,
+    province: "Saskatchewan",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 10,
+    province: "Northwest Territories",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 11,
+    province: "Nunavut",
+    title: "title4",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
+    image: "/images/tree.jpg",
+  },
+  {
+    id: 12,
+    province: "Yukon",
     title: "title4",
     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi error deserunt autem, dolorem tenetur quas corporis a aut ea aliquid.",
     image: "/images/tree.jpg",
   },
 ];
-
-/* <div className="modal-container one">
-  <div className="modal-background">
-    <div className="card card-zoom">
-      <div className="card-content">
-        <div style={{ display: "block" }}>
-          <button className="close-icon " onClick={closeBtn}>
-            <AiOutlineCloseCircle />
-          </button>
-        </div>
-
-        <div className="design card-province">Alberta</div>
-        <div className="content-container">
-          <div className="design content-title">
-            Lorem ipsum dolor sit amet.
-          </div>
-          <div className="design content-img">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo,
-            quae?
-          </div>
-          <div className="design content-desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, a.
-            Modi dolorem voluptates corporis voluptas.
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>; */
-
-/* <>
-  {landmarks
-    .filter((landmark) => landmark.province == props.province)
-    .map((filteredLandmark) => {
-      const { title, image, desc, id, province } = filteredLandmark;
-      console.log(title);
-      return (
-        <div key={id} className="modal-container one">
-          <div className="modal-background">
-            <div className="card card-zoom">
-              <div className="card-content">
-                <div style={{ display: "block" }}>
-                  <button className="close-icon " onClick={closeBtn}>
-                    <AiOutlineCloseCircle />
-                  </button>
-                </div>
-                <div className="design card-province">{province}</div>
-                <div className="content-container">
-                  <div className="design content-title">{title}</div>
-                  <div className="design content-img">
-                    <img src={image} alt="" />
-                  </div>
-                  <div className="design content-desc">{desc}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    })}
-</>; */
