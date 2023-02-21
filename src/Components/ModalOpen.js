55989ff3ae08ee4e0bca721395454b045fe5173e
Fatima Labade <fatima.labade@ontariotechu.net>
2023-02-20 20:05:54 -0500

    icon close

--- src/Components/ModalOpen.js
+++ src/Components/ModalOpen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { landmarks } from "../data";
 import { useGlobalContext } from "../Pages/context";
+import styled from "styled-components";
 
 const ModalOpen = (props) => {
   const { clickClose } = useGlobalContext();
@@ -10,43 +11,55 @@ const ModalOpen = (props) => {
   // console.log(landmarks.title);
   return (
     <>
-      <div className="content">
-        <div className="modal-container one">
-          <div className="modal-background">
-            <div className="card card-zoom">
-              <div className="card-content">
-                <div style={{ display: "block" }}>
-                  <button className="close-icon " onClick={clickClose}>
-                    <AiOutlineCloseCircle />
-                  </button>
-                </div>
-                <div className="design card-province">{props.province}</div>
-                {landmarks
-                  .filter((landmark) => landmark.province === props.province)
-                  .map((filteredLandmark) => {
-                    const { title, image, desc, id, province } =
-                      filteredLandmark;
-                    console.log(title);
-                    return (
-                      //here
+      <Wrapper>
+        <div className='content'>
+          <div className='modal-container one'>
+            <div className='modal-background'>
+              <div className='card card-zoom'>
+                <div className='card-content'>
+                  <div style={{ display: "block" }}>
+                    <button className='close-icon ' onClick={clickClose}>
+                      <AiOutlineCloseCircle />
+                    </button>
+                  </div>
+                  <div className='design card-province'>{props.province}</div>
+                  {landmarks
+                    .filter((landmark) => landmark.province === props.province)
+                    .map((filteredLandmark) => {
+                      const { title, image, desc, id, province } =
+                        filteredLandmark;
+                      console.log(title);
+                      return (
+                        //here
 
-                      // <div className="test">hello world</div>
-                      <div key={id} className="content-container">
-                        <div className="design content-title">{title}</div>
-                        <div className="design content-img">
-                          <img src={image} alt="" />
+                        // <div className="test">hello world</div>
+                        <div key={id} className='content-container'>
+                          <div className='design content-title'>{title}</div>
+                          <div className='design content-img'>
+                            <img src={image} alt='' />
+                          </div>
+                          <div className='design content-desc'>{desc}</div>
                         </div>
-                        <div className="design content-desc">{desc}</div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Wrapper>
     </>
   );
 };
 
 export default ModalOpen;
+
+const Wrapper = styled.div`
+  @media screen and (max-width: 800px) {
+    .map-container {
+      .close-icon {
+        font-size: 0.5rem;
+      }
+    }
+  }
+`;
--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,25 +1,12 @@
 import React from "react";
 import Canada_Svg from "../Components/Canada_Svg";
 
-import styled from "styled-components";
-
 const Map = () => {
   return (
-    <Wrapper>
-      <div className='map-container'>
-        <Canada_Svg />
-      </div>
-    </Wrapper>
+    <div className='map-container'>
+      <Canada_Svg />
+    </div>
   );
 };
 
 export default Map;
-
-const Wrapper = styled.div`
-  @media screen and (max-width: 800px) {
-    .map-container {
-      
-
-    }
-  }
-`;
