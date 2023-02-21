6e0f15ff529be0f17e12cb2b9d5ca7954f08113e
Fatima Labade <fatima.labade@ontariotechu.net>
2023-02-20 20:01:16 -0500

    small screen

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,12 +1,25 @@
 import React from "react";
-// import { useState } from "react";
 import Canada_Svg from "../Components/Canada_Svg";
+
+import styled from "styled-components";
+
 const Map = () => {
   return (
-    <div className="map-container">
-      <Canada_Svg />
-    </div>
+    <Wrapper>
+      <div className='map-container'>
+        <Canada_Svg />
+      </div>
+    </Wrapper>
   );
 };
 
 export default Map;
+
+const Wrapper = styled.div`
+  @media screen and (max-width: 800px) {
+    .map-container {
+      
+
+    }
+  }
+`;
